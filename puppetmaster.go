105a0aecbdbd767f3522b92ee57eeb0fb9b065f1
killermouse0 <github@sakana.fr>
2016-07-26 21:17:40 +0200

    simplify

--- puppetmaster.go
+++ puppetmaster.go
@@ -87,7 +87,7 @@ func main() {
 			switch command {
 			case "/add":
 				log.Println("Message is :", update.Message.Text)
-				ptf.addItems(strings.Fields(update.Message.Text[strings.Index(" ", update.Message.Text):]))
+				ptf.addItems(strings.Fields(update.Message.Text)[1:])
 			case "/del":
 			}
 		}
